8d10872fa19a363972eacb83d74341712aedbf3c
xmoure <ximenamoure@gmail.com>
2025-01-14 12:59:13 -0300

    counter

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import { CartCounter } from "@/app/components/shopping-cart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Counter page',
+  description: 'Contador'
+}
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span> Productos en el carrito</span>
+      <CartCounter value={20} />
+    </div>
+  );
+}
